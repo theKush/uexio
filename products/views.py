502b9bd06b1addd11f1e226beb265a15348debfd
jordanhudgens <jordan@hudgens.com>
2014-03-05 09:40:23 -0600

    integrated additional connections with the view, including a basic permission structure so that users who add products see additional information on the product page than regular users

--- products/views.py
+++ products/views.py
@@ -7,6 +7,17 @@ def list_all(request):
     products = Product.objects.filter(active=True)
     return render_to_response("products/all.html", locals(), context_instance=RequestContext(request))
 
-def single(request):
-    product = Product.objects.all()[0]
+def single(request, slug):
+    product = Product.objects.get(slug=slug)
+    if request.user == product.user:
+        categories = product.category_set.all()
+        context = {
+            "product": product,
+            "categories": categories,
+            "edit": True,
+        }
+    else:
+        context = {
+            "product": product,
+        }
     return render_to_response("products/single.html", locals(), context_instance=RequestContext(request))
