120a466fdf7a6790e4103855a667d85a0f23a9fc
jordanhudgens <jordan@hudgens.com>
2014-03-21 16:51:50 -0500

    integrated the formset factory functionality so that users can upload and edit multiple images per product and placed it into its own html view

--- products/urls.py
+++ products/urls.py
@@ -8,6 +8,7 @@ admin.autodiscover()
 urlpatterns = patterns('products.views',
     url(r'^$', 'list_all', name="all_products"),
     url(r'^add/', 'add_product', name="add_product"),
+    url(r'^(?P<slug>.*)/images/', 'manage_product_image', name="manage_product_image"),
     url(r'^(?P<slug>.*)/edit/', 'edit_product', name="edit_product"),
     url(r'^(?P<slug>.*)/$', 'single', name="single_product"),
 )
--- products/views.py
+++ products/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render_to_response, RequestContext, Http404, HttpResponseRedirect
 from django.template.defaultfilters import slugify
+from django.forms.models import modelformset_factory
+
 
 from .models import Product, Category, ProductImage
-from .forms import ProductForm
+from .forms import ProductForm, ProductImageForm
 
 def list_all(request):
     products = Product.objects.filter(active=True)
@@ -19,6 +21,25 @@ def add_product(request):
             product.save()
             return HttpResponseRedirect('/products/%s'%(product.slug))
         return render_to_response("products/edit.html", locals(), context_instance=RequestContext(request))
+
+
+def manage_product_image(request, slug):
+    try:
+        product = Product.objects.get(slug=slug)
+    except:
+        product = False
+
+    if request.user == product.user:
+        ProductImageFormset = modelformset_factory(ProductImage, form=ProductImageForm)
+        formset = ProductImageFormset(queryset= ProductImage.objects.filter(product__slug=slug))
+        form = ProductImageForm(request.POST or None) # this initializes the ability to reference the image for the product
+
+        if form.is_valid():
+            print "form is valid"
+        return render_to_response("products/manage_images.html", locals(), context_instance=RequestContext(request))
+    else:
+        raise Http404
+
 
 def edit_product(request, slug):
     instance = Product.objects.get(slug=slug)
