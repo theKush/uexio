0e82fc72d4e16c7764f09d6087e206fdb5130bd1
jordanhudgens <jordan@hudgens.com>
2014-04-01 21:24:00 -0500

    integrated the ability to remove items from the cart, also refactored the view method to check to see if any items exist in the cart in order to dynamically render a different view depending on if the cart is empty or if it has items

--- shoppingcart/views.py
+++ shoppingcart/views.py
@@ -1,6 +1,6 @@
 from django.contrib import messages
 from django.shortcuts import render_to_response, Http404, RequestContext, HttpResponseRedirect
-
+from django.core.urlresolvers import reverse
 from products.models import Product
 from products.views import check_product
 
@@ -18,10 +18,15 @@ def shoppingcart(request):
     else:
         shoppingcart = False
 
+    try:
+        exists = ShoppingcartItem.objects.get(shoppingcart=shoppingcart)
+    except:
+        exists = False
+
     return render_to_response('shoppingcart/view_shoppingcart.html', locals(), context_instance=RequestContext(request))
 
 
-def add_to_cart(request, id):
+def update_cart(request, id):
     try:
         product = Product.objects.get(id=id)
     except:
@@ -42,6 +47,11 @@ def add_to_cart(request, id):
     if product:
         new_item, created = ShoppingcartItem.objects.get_or_create(shoppingcart=shoppingcart, product=product)
         if created:
+            new_item.Shoppingcart = shoppingcart
+            new_item.save()
             messages.success(request, 'cart item added')
-        return HttpResponseRedirect('/shoppingcart/')
+        else:
+            new_item.delete()
+            messages.success(request, 'cart item remove')
+        return HttpResponseRedirect(reverse('shoppingcart'))
 
