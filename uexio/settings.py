8a3d3ab859a960b87410585151ca8119953fb73e
Jordan Hudgens <jordan@hudgens.com>
2014-04-12 14:42:44 -0500

    integrated multiple bug fixes for the directory upload structure for images and note features

--- uexio/settings.py
+++ uexio/settings.py
@@ -52,8 +52,8 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "static", "media")
-PROTECTED_UPLOADS = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "static", "protected")
+MEDIA_ROOT = os.path.join(os.path.dirname(os.path.dirname(__file__)), "static", "media")
+PROTECTED_UPLOADS = os.path.join(os.path.dirname(os.path.dirname(__file__)), "static", "protected")
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -64,7 +64,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "static", "static-only")
+STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(__file__)), "static", "static-only")
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -72,7 +72,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "static", "static"),
+    os.path.join(os.path.dirname(os.path.dirname(__file__)), "static", "static"),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
