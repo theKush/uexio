589bc1a6de6b89bdb3cb41b606280d7f97809bfa
Michal Kwiatkowski <michal@trivas.pl>
2014-04-18 22:05:26 +0200

    User profile can now be edited.

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('profiles.views',
     url(r'^$', 'profile', name="profile"),
+    url(r'^edit/$', 'edit_profile', name="edit_profile"),
     url(r'^library/$', 'library', name="library"),
 )
--- profiles/views.py
+++ profiles/views.py
@@ -2,11 +2,23 @@ from django.conf import settings
 from django.shortcuts import render_to_response, RequestContext, Http404, HttpResponseRedirect, HttpResponse
 from django.template.defaultfilters import slugify
 from django.forms.models import modelformset_factory
+from django.core.urlresolvers import reverse
 
 from .models import UserPurchase
+from .forms import EditProfileForm
 
 def profile(request):
     return render_to_response("profiles/profile.html", locals(), context_instance=RequestContext(request))
+
+def edit_profile(request):
+    form = EditProfileForm(request.POST or None, instance=request.user)
+    if request.method == 'POST':
+        try:
+            form.save()
+            return HttpResponseRedirect(reverse('profile'))
+        except ValueError: # handle validation errors
+            pass
+    return render_to_response("profiles/edit_profile.html", locals(), context_instance=RequestContext(request))
 
 def library(request):
     if request.user.is_authenticated():
