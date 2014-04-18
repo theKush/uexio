fe1cdcea313b540c0741e63c159fc3f2a36a7011
Kush Patel <kushpatel@outlook.com>
2014-04-18 16:31:04 -0500

    Update UI for View Profile & gravatar

--- auth/templatetags/gravatar.py
+++ auth/templatetags/gravatar.py
@@ -24,7 +24,7 @@ class GravatarUrlNode(template.Node):
             return ''
 
         default = "http://example.com/static/images/defaultavatar.jpg"
-        size = 40
+        size = 80
 
         gravatar_url = "http://www.gravatar.com/avatar/" + hashlib.md5(email.lower()).hexdigest() + "?"
         gravatar_url += urllib.urlencode({'d':'mm', 's':str(size)})
