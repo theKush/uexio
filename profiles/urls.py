8a94d7eb300660f15360d11abcc8bf7fad35a9d1
Michal Kwiatkowski <michal@trivas.pl>
2014-04-19 14:15:33 +0200

    Made user profiles public, each with a unique URL based on the username.

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('profiles.views',
-    url(r'^$', 'profile', name="profile"),
+    url(r'^public/(?P<username>.*)$', 'profile', name="profile"),
     url(r'^edit/$', 'edit_profile', name="edit_profile"),
     url(r'^library/$', 'library', name="library"),
     url(r'^listings/$', 'listings', name="listings"),
--- profiles/views.py
+++ profiles/views.py
@@ -3,25 +3,32 @@ from django.shortcuts import render_to_response, RequestContext, Http404, HttpRe
 from django.template.defaultfilters import slugify
 from django.forms.models import modelformset_factory
 from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
 
 from profiles.models import Product
 from .models import UserPurchase
 from .forms import EditProfileForm
 
-def profile(request):
+def profile(request, username):
+    current_user_profile_url = get_current_user_profile_url(request)
+    profile_user = User.objects.get(username=username)
+    if not profile_user:
+        raise Http404
     return render_to_response("profiles/profile.html", locals(), context_instance=RequestContext(request))
 
 def edit_profile(request):
+    current_user_profile_url = get_current_user_profile_url(request)
     form = EditProfileForm(request.POST or None, instance=request.user)
     if request.method == 'POST':
         try:
             form.save()
-            return HttpResponseRedirect(reverse('profile'))
+            return HttpResponseRedirect(reverse('profile', args=[request.user.username]))
         except ValueError: # handle validation errors
             pass
     return render_to_response("profiles/edit_profile.html", locals(), context_instance=RequestContext(request))
 
 def library(request):
+    current_user_profile_url = get_current_user_profile_url(request)
     if request.user.is_authenticated():
         products = request.user.purchased_products()
         return render_to_response("profiles/library.html", locals(), context_instance=RequestContext(request))
@@ -29,5 +36,9 @@ def library(request):
         raise Http404
 
 def listings(request):
+    current_user_profile_url = get_current_user_profile_url(request)
     products = Product.objects.filter(user=request.user)
     return render_to_response("profiles/listings.html", locals(), context_instance=RequestContext(request))
+
+def get_current_user_profile_url(request):
+    return reverse('profile', args=[request.user.username])
