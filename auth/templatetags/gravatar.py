b597cf4d1aa7f4cdab3d0647be1b87a39a552667
Michal Kwiatkowski <michal@trivas.pl>
2014-04-19 14:31:37 +0200

    Using gravar as user's profile picture.

---
+++ auth/templatetags/gravatar.py
@@ -0,0 +1,11 @@
+from django import template
+import hashlib
+
+register = template.Library()
+
+@register.simple_tag
+def gravatar_image_url(user, size=80):
+    md5 = hashlib.md5()
+    md5.update(user.email.strip().lower())
+    hash = md5.hexdigest()
+    return "http://www.gravatar.com/avatar/%s?s=%s" % (hash, size)
