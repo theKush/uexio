ab7aefbb404d1842467e456d9deb44bd63c9c5b9
Kush Patel <kushpatel@outlook.com>
2014-04-24 20:37:33 -0500

    Added Static pages for the footer and their url routing

--- uexio/urls.py
+++ uexio/urls.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
-
+from django.views.generic import TemplateView
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -8,7 +8,14 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # regular expression below sets up the path for the app, more info in settings
     url(r'^$', 'pages.views.index', name="index"),
-
+    url(r'^aboutus/', TemplateView.as_view(template_name='pages/aboutus.html')),
+    url(r'^howtobuy/', TemplateView.as_view(template_name='pages/how_to_buy.html')),
+    url(r'^howtosell/', TemplateView.as_view(template_name='pages/how_to_sell.html')),
+    url(r'^contactus/', TemplateView.as_view(template_name='pages/contact_us.html')),
+    url(r'^faq/', TemplateView.as_view(template_name='pages/faq.html')),
+    url(r'^privacy/', TemplateView.as_view(template_name='pages/privacy.html')),
+    url(r'^termsofuse/', TemplateView.as_view(template_name='pages/terms_of_use.html')),
+    url(r'^vslocalbookstore/', TemplateView.as_view(template_name='pages/vs_local_bookstore.html')),
     url(r'^auth/', include('auth.urls')),
     url(r'^profile/', include('profiles.urls')),
     url(r'^products/', include('products.urls')),
