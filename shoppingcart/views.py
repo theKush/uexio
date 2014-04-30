cf26f3494ddd2b7e8b8572d3d3cd8bf3ba0fe5f6
Michal Kwiatkowski <michal@trivas.pl>
2014-04-30 08:36:16 +0200

    Refactored shopping cart access functions.

--- shoppingcart/views.py
+++ shoppingcart/views.py
@@ -8,7 +8,7 @@ from .forms import ApplyCouponForm
 
 def shoppingcart(request):
     coupon_form = ApplyCouponForm()
-    items = _get_shooping_cart_items(request)
+    items = _get_shopping_cart_items(request)
     total_price = sum(item.product.price for item in items)
     total_discount = sum(item.discount or 0 for item in items)
     total_after_discount = total_price - total_discount
@@ -21,17 +21,7 @@ def update_cart(request, id):
     except:
         product = False
 
-    try:
-        shoppingcart_id = request.session['shoppingcart_id']
-    except:
-        shoppingcart_id = False
-
-    try:
-        shoppingcart = Shoppingcart.objects.get(id=shoppingcart_id)
-    except Shoppingcart.DoesNotExist:
-        shoppingcart = Shoppingcart()
-        shoppingcart.save()
-        request.session['shoppingcart_id'] = shoppingcart.id
+    shoppingcart = _get_or_create_shopping_cart(request)
 
     if product:
         new_item, created = ShoppingcartItem.objects.get_or_create(shoppingcart=shoppingcart, product=product)
@@ -52,7 +42,7 @@ def apply_coupon(request):
                 applied = False
                 code = form.cleaned_data['code']
                 coupon = Coupon.objects.get(code=code)
-                items = _get_shooping_cart_items(request)
+                items = _get_shopping_cart_items(request)
                 for item in items:
                     if item.product == coupon.product:
                         item.discount = coupon.discount
@@ -65,10 +55,25 @@ def apply_coupon(request):
                 messages.error(request, 'Coupon code invalid')
     return HttpResponseRedirect(reverse('shoppingcart'))
 
-def _get_shooping_cart_items(request):
+def _get_shopping_cart_items(request):
+    shoppingcart = _get_shopping_cart(request)
+    if shoppingcart:
+        return shoppingcart.shoppingcartitem_set.all()
+    else:
+        return []
+
+def _get_shopping_cart(request):
     try:
         shoppingcart_id = request.session['shoppingcart_id']
         shoppingcart = Shoppingcart.objects.get(id=shoppingcart_id)
-        return shoppingcart.shoppingcartitem_set.all()
-    except KeyError:
-        return []
+        return shoppingcart
+    except (KeyError, Shoppingcart.DoesNotExist):
+        return None
+
+def _get_or_create_shopping_cart(request):
+    shoppingcart = _get_shopping_cart(request)
+    if not shoppingcart:
+        shoppingcart = Shoppingcart()
+        shoppingcart.save()
+        request.session['shoppingcart_id'] = shoppingcart.id
+    return shoppingcart
