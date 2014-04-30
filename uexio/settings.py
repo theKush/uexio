a04780a4d6890dfc86a086ab9c713f7e344f7dda
Michal Kwiatkowski <michal@trivas.pl>
2014-04-30 12:04:05 +0200

    Provide default for development database.

--- uexio/settings.py
+++ uexio/settings.py
@@ -155,9 +155,13 @@ LOGGING = {
 
 DATABASES = {}
 
+if not os.environ.get('DATABASE_URL'):
+    os.environ['DATABASE_URL'] = 'sqlite:///uexio.sqlite3'
+
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
+
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
