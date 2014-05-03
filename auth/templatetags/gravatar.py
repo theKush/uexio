a56ecccdf51a6efce635a1d1fe964d66239cd908
Kush Patel <kushpatel@outlook.com>
2014-05-02 19:34:33 -0500

    Update gravetar default image to be mystery man

--- auth/templatetags/gravatar.py
+++ auth/templatetags/gravatar.py
@@ -8,4 +8,4 @@ def gravatar_image_url(user, size=80):
     md5 = hashlib.md5()
     md5.update(user.email.strip().lower())
     hash = md5.hexdigest()
-    return "http://www.gravatar.com/avatar/%s?s=%s" % (hash, size)
+    return "http://www.gravatar.com/avatar/%s?s=%s&d=mm" % (hash, size)
